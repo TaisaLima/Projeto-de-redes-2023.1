b24902a2349b465edfd56f14ac3377d27b9f55da
Matheus Ryan <mrysn@ic.ufal.br>
2023-10-03 00:00:55 -0300

    Update servidor

--- src/servidor.py
+++ src/servidor.py
@@ -5,29 +5,29 @@ import threading
 
 def receive_files(conn):
 
-    # CONFIRMAR ARQUIVO
+    # CONFIRMAR ARQUIVO ATRAVÉS DO CLIENTE
     try:
-        confirm = conn.recv(4096).rstrip().decode()
-        file_name = confirm.split(":")[-1]
-        file_size = int(confirm.split(":")[0])
+        confirm = conn.recv(4096).rstrip().decode() #RECEBE A CONFIRMAÇÃO
+        file_name = confirm.split(":")[-1] #PEGA O NOME DO ARQUIVO
+        file_size = int(confirm.split(":")[0]) #PEGA O TAMANHO DO ARQUIVO
 
-    except Exception as error:
+    except Exception as error: #EXCEÇÃO CASO O ARQUIVO NÃO SEJA ENCONTRADO
         print("\nErro na confirmacao:", error)
         return
 
     aux_size = file_size
-    # CASO JA EXISTA UM ARQUIVO DE MESMO NOME NA PASTA
+    # OCORRÊNCIA DE ARQUIVOS DE MESMO NOME
     aux_name = file_name
     i = 1
     while os.path.exists("../dados/" + aux_name):
-        aux_name = '(' + str(i) + ') ' + file_name
+        aux_name = '(' + str(i) + ') ' + file_name #ADICIONA PARÊNTESIS 
         i += 1
 
     file_name = aux_name
 
-    # BAIXANDO ARQUIVO
+    # REALIZANDO O DOWNLOAD DO ARQUIVO
     with open("../dados/" + file_name, 'wb') as file:
-        while True:
+        while True: #LAÇo PARA A BAIXAR O ARQUIVO
             if aux_size <= 0:
                 break
 
@@ -43,9 +43,9 @@ def receive_files(conn):
     print("\nArquivo adicionado com sucesso!")
 
 
-def list_files(conn):
+def list_files(conn): #FUNÇÃO PARA LISTAR OS ARQUIVOS DENTRO DO DIRETÓRIO
 
-    files = os.listdir(os.path.join(os.getcwd(), "../dados"))
+    files = os.listdir(os.path.join(os.getcwd(), "../dados")) #LISTA OS ARQUIVOS
     options = "Opções de download:\n"
     i = 1
 
@@ -54,22 +54,22 @@ def list_files(conn):
         i += 1
 
     try:
-        conn.sendall(str.encode(options))
+        conn.sendall(str.encode(options)) #ENVIA A LISTA DE ARQUIVOS COMO UMA STRING
     except Exception as error:
         print("Erro no envio:", error)
 
 
-def send_files(conn):
-    files = os.listdir(os.path.join(os.getcwd(), "../dados"))
+def send_files(conn): #FUNÇÃO PARA ENVIAR OS ARQUIVOS
+    files = os.listdir(os.path.join(os.getcwd(), "../dados")) #LISTA OS ARQUIVOS EM UM VETOR
 
     # RECEBENDO OPCAO
     try:
-        resp_option = int(conn.recv(1024).rstrip().decode())
+        resp_option = int(conn.recv(1024).rstrip().decode()) #RECEBE A OPÇÃO ESCOLHIDA PELO CLIENTE
         file_size = os.path.getsize(
             "../dados/" + files[resp_option-1])
-        confirm = str(file_size) + ":" + \
+        confirm = str(file_size) + ":" + \       
             str(files[resp_option-1])
-        conn.sendall(confirm.encode())
+        conn.sendall(confirm.encode())#ENVIA A CONFIRMAÇÃO DO ARQUIVO
 
     except Exception as error:
         print("Erro no recebimento de dados:", error)
@@ -78,20 +78,20 @@ def send_files(conn):
     # ENVIANDO ARQUIVO
     try:
         choosed_file = open(
-            "../dados/" + str(files[resp_option-1]), "rb")
+            "../dados/" + str(files[resp_option-1]), "rb") #ESCOLHE O ARQUIVO
 
         while True:
-            file_in_bytes = choosed_file.read(1024)
+            file_in_bytes = choosed_file.read(1024) #LÊ E ENVIA CADA BYTE DOS ARQUIVOS
             if len(file_in_bytes) <= 0:
                 # FINALIZOU O ARQUIVO
                 break
-            conn.sendall(file_in_bytes)
+            conn.sendall(file_in_bytes) # ENVIA O BYTE
 
     except Exception as error:
         print("Erro no envio de dados:", error)
 
 
-def client_control(conn, client_addr):
+def client_control(conn, client_addr): #MENU
 
     while True:
         option = conn.recv(1024).decode()
